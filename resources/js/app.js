c3b99a7a32e5b5af7e3fcc385aaafcb7362438cd
KevinCeferino <kevin.ceferino000@gmail.com>
2022-03-14 11:13:45 -0500

    First Commit - Home

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import 'particles.js/particles';
+const particlesJS = window.particlesJS;
+
+particlesJS.load('home', 'json/particlesjs-config.json', function() {
+    console.log('callback - particles-js config loaded');
+  });
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,9 @@
+<div class="sidebar">
+    <ul class="nav-menu">
+        <li class="nav-item"><a href="/" class="nav-item-link">About</a></li>
+        <li class="nav-item"><a href="/" class="nav-item-link">Skills</a></li>
+        <li class="nav-item"><a href="/" class="nav-item-link">Projects</a></li>
+        <li class="nav-item"><a href="/" class="nav-item-link">Blog</a></li>
+        <li class="nav-item"><a href="/" class="nav-item-link">Contact</a></li>
+    </ul>
+</div>
